9417253fda8f4ce104f56c1e078500f14b2134ba
Nikita Markovkin <nikmark1212@gmail.com>
2021-07-12 20:36:32 +0300

    MultithreadedCounter

--- src/homework11/MultithreadedCounter.java
+++ src/homework11/MultithreadedCounter.java
@@ -9,26 +9,26 @@ public class MultithreadedCounter implements Runnable{
 
     public static void main(String[] args) {
 
-        MultithreadedCounter te = new MultithreadedCounter();
-        Thread th1 = new Thread(te);
+        MultithreadedCounter mtc = new MultithreadedCounter();
+        Thread th1 = new Thread(mtc);
         th1.start();
-        Thread th2 = new Thread(te);
+        Thread th2 = new Thread(mtc);
         th2.start();
-        Thread th3 = new Thread(te);
+        Thread th3 = new Thread(mtc);
         th3.start();
-        Thread th4 = new Thread(te);
+        Thread th4 = new Thread(mtc);
         th4.start();
-        Thread th5 = new Thread(te);
+        Thread th5 = new Thread(mtc);
         th5.start();
-        Thread th6 = new Thread(te);
+        Thread th6 = new Thread(mtc);
         th6.start();
-        Thread th7 = new Thread(te);
+        Thread th7 = new Thread(mtc);
         th7.start();
-        Thread th8 = new Thread(te);
+        Thread th8 = new Thread(mtc);
         th8.start();
-        Thread th9 = new Thread(te);
+        Thread th9 = new Thread(mtc);
         th9.start();
-        Thread th10 = new Thread(te);
+        Thread th10 = new Thread(mtc);
         th10.start();
     }
 
